2f6871021e3fe004c53a5ac3036645b00dec2814
ClarXo <imp_rove@naver.com>
2022-01-25 11:15:49 +0900

    test(peek): stack returns item from top

--- src/test/stack.test.ts
+++ src/test/stack.test.ts
@@ -36,4 +36,23 @@ describe("Stack", () => {
       expect(sizeAfterPopped).toBe(1);
     });
   });
+
+  describe("peek", () => {
+    it("throws error without item", () => {
+      expect(() => stack.peek()).toThrowError("Null pointer exception");
+    });
+
+    it("returns item from top but don't remove it from stack", () => {
+      stack.push("first item");
+      stack.push("second item");
+
+      const sizeAfterStacked = stack.getSize();
+      const item = stack.peek();
+      const sizeAfterPopped = stack.getSize();
+
+      expect(sizeAfterStacked).toBe(2);
+      expect(item).toBe("second item");
+      expect(sizeAfterPopped).toBe(2);
+    });
+  });
 });
