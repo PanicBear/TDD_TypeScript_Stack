3ffcecefea2681600b52f6ee1a319c7806511ff3
ClarXo <imp_rove@naver.com>
2022-01-25 11:12:15 +0900

    test(pop): stack returns item and remove from top

--- src/test/stack.test.ts
+++ src/test/stack.test.ts
@@ -17,4 +17,23 @@ describe("Stack", () => {
     const size = stack.getSize();
     expect(size).toBe(1);
   });
+
+  describe("pop", () => {
+    it("throws error without item", () => {
+      expect(() => stack.pop()).toThrowError("Null pointer exception");
+    });
+
+    it("returns item from top and remove it from stack", () => {
+      stack.push("first item");
+      stack.push("second item");
+
+      const sizeAfterStacked = stack.getSize();
+      const item = stack.pop();
+      const sizeAfterPopped = stack.getSize();
+
+      expect(sizeAfterStacked).toBe(2);
+      expect(item).toBe("second item");
+      expect(sizeAfterPopped).toBe(1);
+    });
+  });
 });
